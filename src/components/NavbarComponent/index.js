318244e64881134dedd727cde5dbe15ce495a17d
ugurkiymetli <ugurkiymetli@hotmail.com>
2021-11-07 18:44:44 +0300

    post detail improved with bootstrap card component

--- src/components/NavbarComponent/index.js
+++ src/components/NavbarComponent/index.js
@@ -1,61 +1,50 @@
 import { Link } from "react-router-dom";
-import { Button } from "@chakra-ui/react";
+import { Button, Image } from "@chakra-ui/react";
 import logo from "../../gelecek-full.svg";
 import { Navbar, Nav } from "react-bootstrap";
 
 function NavbarComponent() {
   return (
     <>
-      <Navbar sticky="top" bg="light" expand="md">
+      <Navbar sticky="top" bg="light" className="px-2" expand="md">
         <Navbar.Brand as={Link} to="/gelecek-varlik-hr/">
-          <img src={logo} alt="gelecek-varlik-logo" />
+          <Image src={logo} alt="gelecek-varlik-logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="ms-1" id="basic-navbar-nav">
+        <Navbar.Collapse
+          className="ms-1 justify-content-end"
+          id="basic-navbar-nav"
+        >
           <Nav className="m">
-            <Nav.Link as={Link} to="#">
-              <Button variant="Link">Açık Pozisyonlar</Button>
+            <Nav.Link as={Link} to="/gelecek-varlik-hr/">
+              <Button boxShadow="lg" rounded="md" variant="Link">
+                Açık Pozisyonlar
+              </Button>
             </Nav.Link>
             <Nav.Link as={Link} to="#">
-              <Button variant="Link">Başvurularım</Button>
+              <Button boxShadow="lg" rounded="md" variant="Link">
+                Başvurularım
+              </Button>
             </Nav.Link>
             <Nav.Link as={Link} to="#">
-              <Button variant="Link">Özgeçmiş Bilgilerim</Button>
+              <Button boxShadow="lg" rounded="md" variant="Link">
+                Özgeçmiş Bilgilerim
+              </Button>
             </Nav.Link>
           </Nav>
           <Link to="#">
-            <Button color="#fff" backgroundColor="#f5a623" variant="outline">
+            <Button
+              color="#fff"
+              backgroundColor="#f5a623"
+              boxShadow="lg"
+              rounded="md"
+              variant="solid"
+            >
               Giriş Yap
             </Button>
           </Link>
         </Navbar.Collapse>
       </Navbar>
-
-      {/* <nav className={styles.nav}>
-        <div className={styles.left}>
-          <div className={styles.logo}>
-            <Link to="/gelecek-varlik-hr/">
-              <img src={logo} alt="gelecek-varlik-logo" />
-            </Link>
-          </div>
-          <ul className={styles.menu}>
-            <li>
-              <Link to="#">Açık Pozisyonlar</Link>
-            </li>
-
-            <li>
-              <Link to="#">Başvurularım </Link>
-            </li>
-
-            <li>
-              <Link to="#">Özgeçmiş Bilgilerim</Link>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.right}>
-          <Button variant="solid">Giriş Yap</Button>
-        </div>
-      </nav> */}
     </>
   );
 }
--- src/components/Posts/PostDetail.js
+++ src/components/Posts/PostDetail.js
@@ -1,28 +1,48 @@
 import React from "react";
 import { usePost } from "../../context/PostsContext";
 import { useParams } from "react-router-dom";
-import { Heading, Box, Text } from "@chakra-ui/react";
-
+import { Heading, Box, Text, Button } from "@chakra-ui/react";
+import { Card } from "react-bootstrap";
 function PostDetail() {
   const { posts } = usePost();
   const { post_id } = useParams();
   const post = posts[post_id - 1];
-  console.log("post id:", post_id);
+  /* console.log("post id:", post_id); */
+  /* const date = new Date(post.date);
+  console.log(date); */
   return (
-    <Box width="80%" mx="auto" mt="4">
-      <Heading mb="2">İlan Detayı</Heading>
-      <Text fontSize="larger" mb="1">
-        {post.title}
-      </Text>
-      <hr />
-      <Text fontSize="md">
-        {post.poster}-{post.date}
-      </Text>
-      <Text>Şehir: {post.city} </Text>
-      <br />
-      <Text>Genel Nitelikleri</Text>
-      <Text>{post.description}</Text>
-    </Box>
+    <>
+      <Card style={{ borderColor: "#f5a623" }} className="text-center m-3 p-1">
+        <Card.Header>
+          <Heading as="h4" size="md">
+            İş İlanı
+          </Heading>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>
+            <Heading as="h5" size="sm">
+              {post.title}
+            </Heading>
+          </Card.Title>
+          <Card.Subtitle className="text-muted">
+            Şehir: {post.city}
+          </Card.Subtitle>
+          <hr />
+          <Card.Text className="mt-2">{post.description}</Card.Text>
+          <Button
+            mt="2"
+            color="#fff"
+            backgroundColor="#f5a623"
+            variant="outline"
+          >
+            Başvur
+          </Button>
+        </Card.Body>
+        <Card.Footer className="text-muted mt-2">
+          {post.poster} - {post.date}
+        </Card.Footer>
+      </Card>
+    </>
   );
 }
 
